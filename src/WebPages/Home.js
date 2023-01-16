8234ceb618d82885562f0aa6a338bbfd54fed7b2
Hossein Hajmirbaba <h.hajmirbaba@gmail.com>
2023-01-16 13:20:51 -0500

    this is commits for not commits

--- src/WebPages/Home.js
+++ src/WebPages/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
   return (
-    <div className="homeRows">
-      <div className="">Home is home</div>
+    <div className="homePage">
+      <div className="">
+        <h1 className="introHome">
+          Hi, I'm Hossein. <br /> I'm a
+          <span style={{ color: "#0469FF" }}> software engineer </span> <br />
+          student in Ottawa.
+        </h1>
+      </div>
+      <div className="buttons">
+        <a href="/About"><button type="button" className=" homeBtn btn" >About Me</button></a>
+        <a href="/Work"><button type="button" className=" homeBtn btn" >My Work</button></a>
+        <a href="/Contact"><button type="button" className=" homeBtn btn" >Contact Me</button></a>
+      </div>
     </div>
   );
 };
